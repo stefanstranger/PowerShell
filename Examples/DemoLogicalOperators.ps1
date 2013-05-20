eefdc695cfba50591314082a93fcb4c37692b98b
Stefan Stranger <stefan@stranger.nl>
2013-05-20 14:57:17 +0300

    Update DemoLogicalOperators.ps1

--- Examples/DemoLogicalOperators.ps1
+++ Examples/DemoLogicalOperators.ps1
@@ -4,6 +4,6 @@
 # operator is $false, the expression on the right-hand is not evaluated.
 
 #Compare the differences
-if ((5 -lt 4) -and (1..10000 | % {if ($_ -eq "never"){}})){} #take no long because left-hand is false
+if ((5 -lt 4) -and (1..10000 | % {if ($_ -eq "never"){}})){} #takes not long because left-hand is false
 
 if ((3 -lt 4) -and (1..10000 | % {if ($_ -eq "never"){}})){} #take some time to finish
