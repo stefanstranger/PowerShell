f71bd3890ad222361b7bf785fb0a194531172390
Stefan Stranger <stefan.stranger@outlook.com>
2013-05-20 13:50:51 +0200

    Initial version of Logical Operator Example

---
+++ Examples/DemoLogicalOperators.ps1
@@ -0,0 +1,9 @@
+﻿# Demo Logical Operators
+# Comparison Operators are short circuit operators.
+# This means that if the expression on the left-hand side of the -end
+# operator is $false, the expression on the right-hand is not evaluated.
+
+#Compare the differences
+if ((5 -lt 4) -and (1..10000 | % {if ($_ -eq "never"){}})){} #take no long because left-hand is false
+
+if ((3 -lt 4) -and (1..10000 | % {if ($_ -eq "never"){}})){} #take some time to finish
