0464085d78879e0727fc4ccd0f475d01b0166d22
Stefan Stranger <stefan.stranger@outlook.com>
2013-05-31 21:38:31 +0200

    test script to debug DNSSec

---
+++ DNSSec_v0.2.ps1
@@ -0,0 +1,42 @@
+﻿<#
+.Synopsis
+   DNSSec Debugger using Verisign webpage
+.DESCRIPTION
+   The DNSSEC Debugger is a Web-based tool for ensuring that the "chain of trust" is intact for a particular DNSSEC enabled domain name. 
+   The tool shows a step-by-step validation of a given domain name and highlights any problems found.
+.EXAMPLE
+   Test-DNSSec -DomainName www.verisign.com
+.EXAMPLE
+   Test-DNSSec -DomainName www.stranger.nl
+.LINKS
+   http://dnssec-debugger.verisignlabs.com/
+#>
+function Test-Dnssec
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   Position=0)]
+        $DomainName
+    )
+
+    $url = "http://dnssec-debugger.verisignlabs.com/$DomainName"
+
+    $result = Invoke-WebRequest $URL
+
+    $result.ParsedHtml.getElementsByTagName("tr") | 
+        Where-Object "classname" -match "^L0" | 
+            Select-Object -ExpandProperty InnerText | 
+                ForEach-Object {if ($_ -match "^No")
+                    {
+                        write-host $_ -ForegroundColor red
+                    }
+                    else 
+                    {
+                        Write-Host $_
+                    }
+                }
+}
+
