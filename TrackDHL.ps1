5f47db5e4487d6e63ffd994705cec1d9c9d79ec4
Stefan Stranger <stefan.stranger@outlook.com>
2013-12-18 21:41:59 +0100

    Initial Track DHL Package
    Track DHL Packages using PowerShell

---
+++ TrackDHL.ps1
@@ -0,0 +1,7 @@
+﻿$trackingnumber = read-host "Enter DHL Tracking number"
+
+$url = "http://www.dhl-usa.com/content/us/en/express/tracking.shtml?brand=DHL&AWB=$trackingnumber"
+
+$result = Invoke-WebRequest -Uri $url
+
+$result.ParsedHtml.getElementsByTagName("table") | select -expandProperty OuterText
