9e03927924bdca8dde4c7457ce071dda071c746a
Stefan Stranger <stefan.stranger@outlook.com>
2014-02-26 10:20:22 +0100

    Added header and found a bug.

--- Examples/ISEAdOn_Example.ps1
+++ Examples/ISEAdOn_Example.ps1
@@ -1,6 +1,15 @@
-﻿$header = @"
+﻿# ---------------------------------------------------
+# Script: C:\Users\stefstr\Documents\GitHub\PowerShell\Examples\ISEAdOn_Example.ps1
+# Version:
+# Author: Stefan Stranger
+# Date: 02/26/2014 10:18:13
+# Description:
+# Comments: Current file is currently not working. It's retrieving the value of the script that loads the add-on.
 # ---------------------------------------------------
-# Script: $($psise.CurrentFile.FullPath)
+
+$header = @"
+# ---------------------------------------------------
+# Script: $($psise.currentfile.fullpath)
 # Version:
 # Author: Stefan Stranger
 # Date: $(get-date)
@@ -15,5 +24,3 @@
     SubMenus.
     Add("Insert Standard Header",
 {$PSISE.CurrentFile.Editor.InsertText($header)} , "ALT+H") 
-
-
