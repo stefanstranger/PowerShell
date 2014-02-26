73ceb3119071f43b70076d48097b05f901543e3a
Stefan Stranger <stefan.stranger@outlook.com>
2014-02-26 12:49:21 +0100

    Updated Header

--- Examples/ISEAdOn_Example.ps1
+++ Examples/ISEAdOn_Example.ps1
@@ -2,9 +2,10 @@
 # Script: C:\Users\stefstr\Documents\GitHub\PowerShell\Examples\ISEAdOn_Example.ps1
 # Version:
 # Author: Stefan Stranger
-# Date: 02/26/2014 10:18:13
+# Date: 02/26/2014 12:48:19
 # Description:
 # Comments: Current file is currently not working. It's retrieving the value of the script that loads the add-on.
+# Fixes: Fixed issues with not updating Date and showing currect file location.
 # ---------------------------------------------------
 
 [void]$psise.
