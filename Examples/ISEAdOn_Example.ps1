06e57585033a9eaece93bfe586ee16bde273a7d0
Stefan Stranger <stefan.stranger@outlook.com>
2014-02-26 12:47:51 +0100

    Fixed the issues
    I've now used a seperate script to run when the ISEAddon is called.

--- Examples/ISEAdOn_Example.ps1
+++ Examples/ISEAdOn_Example.ps1
@@ -7,20 +7,9 @@
 # Comments: Current file is currently not working. It's retrieving the value of the script that loads the add-on.
 # ---------------------------------------------------
 
-$header = @"
-# ---------------------------------------------------
-# Script: $($psise.currentfile.fullpath)
-# Version:
-# Author: Stefan Stranger
-# Date: $(get-date)
-# Description:
-# Comments:
-# ---------------------------------------------------
-"@
-
 [void]$psise.
     CurrentPowerShellTab.
     AddOnsMenu.
     SubMenus.
     Add("Insert Standard Header",
-{$PSISE.CurrentFile.Editor.InsertText($header)} , "ALT+H") 
+{C:\Users\stefstr\Documents\GitHub\PowerShell\Examples\scriptheader.ps1;$PSISE.CurrentFile.Editor.InsertText($header)} , "ALT+H") 
---
+++ Examples/ScriptHeader.ps1
@@ -0,0 +1,10 @@
+﻿$global:header = @"
+# ---------------------------------------------------
+# Script: $($psise.currentfile.fullpath)
+# Version:
+# Author: Stefan Stranger
+# Date: $(get-date)
+# Description:
+# Comments:
+# ---------------------------------------------------
+"@
