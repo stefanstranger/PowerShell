51f7cd347dc4f5e2524813c84ef9bb4931d098c1
Stefan Stranger <stefan.stranger@outlook.com>
2014-02-26 10:04:26 +0100

    New Examples

---
+++ Examples/ISEAdOn_Example.ps1
@@ -0,0 +1,19 @@
+﻿$header = @"
+# ---------------------------------------------------
+# Script: $($psise.CurrentFile.FullPath)
+# Version:
+# Author: Stefan Stranger
+# Date: $(get-date)
+# Description:
+# Comments:
+# ---------------------------------------------------
+"@
+
+[void]$psise.
+    CurrentPowerShellTab.
+    AddOnsMenu.
+    SubMenus.
+    Add("Insert Standard Header",
+{$PSISE.CurrentFile.Editor.InsertText($header)} , "ALT+H") 
+
+
