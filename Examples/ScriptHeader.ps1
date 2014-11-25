b0b74d48a80a1cb855105dd5c77440c1093ada26
Stefan Stranger <stefan.stranger@outlook.com>
2014-11-25 09:47:10 +0100

    Disclamer added
    Added Disclamer for ScriptHeader scripts

--- Examples/ScriptHeader.ps1
+++ Examples/ScriptHeader.ps1
@@ -6,5 +6,10 @@
 # Date: $(get-date)
 # Description:
 # Comments:
+# Changes:  
+# Disclaimer: 
+# This example is provided “AS IS” with no warranty expressed or implied. Run at your own risk. 
+# **Always test in your lab first**  Do this at your own risk!! 
+# The author will not be held responsible for any damage you incur when making these changes!
 # ---------------------------------------------------
 "@
