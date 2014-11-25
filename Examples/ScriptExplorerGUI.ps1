727309f87b1763782457cf948c77106d60b74e2b
Stefan Stranger <stefan.stranger@outlook.com>
2014-11-25 09:46:28 +0100

    Script Explorer GUI
    GUI using Out-GridView to search for scripts and open them in the ISE

---
+++ Examples/ScriptExplorerGUI.ps1
@@ -0,0 +1,18 @@
+﻿# ---------------------------------------------------
+# Script: C:\Scripts\PS\ScriptExplorerGUI.ps1
+# Version: 0.1
+# Author: Stefan Stranger
+# Date: 11/25/2014 09:43:27
+# Description: GUI for PowerShell scripts
+# Comments:
+# Changes:  
+# Disclaimer: 
+# This example is provided “AS IS” with no warranty expressed or implied. Run at your own risk. 
+# **Always test in your lab first**  Do this at your own risk!! 
+# The author will not be held responsible for any damage you incur when making these changes!
+# ---------------------------------------------------
+ISE -File (
+    Get-ChildItem -Path C:\Scripts\PS\*.ps1 -Recurse  |  
+    Out-GridView -OutputMode Single -Title "Select PowerShell Script to Open:" |
+    Select-Object -ExpandProperty FullName
+)
