98ebee7b3ea10f34302918229824c71cc26194a9
Stefan Stranger <stefan@stranger.nl>
2015-02-14 14:36:38 +0100

    Improved WinMerge_WPF script
    Changed location winmerge and some visual improvements.

--- WinMergeISEAddOn/WINMerge_WPF.ps1
+++ WinMergeISEAddOn/WINMerge_WPF.ps1
@@ -1,11 +1,11 @@
 ﻿# ---------------------------------------------------
 # Script: C:\Users\stefstr\Documents\GitHub\PowerShell\WinMergeISEAddOn\WINMerge_WPF.ps1
-# Version: 0.1
+# Version: 0.2
 # Author: Stefan Stranger
-# Date: 02/12/2015 14:50:36
+# Date: 02/14/2015 14:50:36
 # Description: PowerShell script to generate WPF form for WinMerge-ISEAddOn
 # Comments: Manually install WinMerge
-# Changes:  
+# Changes: Changed path to WinMerge.
 # Disclaimer: 
 # This example is provided “AS IS” with no warranty expressed or implied. Run at your own risk. 
 # **Always test in your lab first**  Do this at your own risk!! 
@@ -13,14 +13,28 @@
 # ---------------------------------------------------
 ipmo showui
 
+# Variables
 $tabs = $PSISe.PowerShellTabs.Files
 $totalrows = $tabs.count + 3
-$tabsrow = 1 
+$tabsrow = 1
+#Configure the path the location of WinMergeU.exe application.
+$exe = "C:\Program Files (x86)\WinMerge\WinMergeU.exe"
 
-Set-UIStyle "L1" @{ FontFamily = "Helvetica"; FontSize = 14; FontWeight = "Bold" }
+#WPF Object Styles
+Set-UIStyle "L1" @{ FontFamily = "Helvetica"; FontSize = 14; FontWeight = "Bold"; ForeGround = "#6E0504" }
 Set-UIStyle "C1" @{ FontFamily = "Helvetica"; FontSize = 12 }
 
-New-Grid -Name WinMerge -ControlName 'Show-ISETab' -Rows $totalrows -Columns 2 -Show -On_Loaded {
+# Grid Style
+$gridparam = @{
+    Name = "WinMerge"
+    ControlName = "Show-ISETab"
+    Background = '#FFD82F' #WinMerge color
+    Rows = $totalrows
+    Columns =  2
+}
+
+
+New-Grid @gridparam -Show -On_Loaded {
     $window.title = "WinMerge ISE add-on" } {
     New-Label "Select 2 Open ISE tabs" -Row 0 -Column 0 -VisualStyle L1
 
@@ -28,9 +42,9 @@ New-Grid -Name WinMerge -ControlName 'Show-ISETab' -Rows $totalrows -Columns 2 -
     foreach ($tab in $tabs)
 
         {
+            #Remove non ascii characters from open filenames. Checkbox objects cannot have non-ascii characters in their name.
             $name = ($tab.Displayname) -creplace '[^a-zA-Z0-9]', ''
             New-Checkbox -Name $name -Content $tab.displayname -DataContext $tab -BorderThickness 1 -Row $tabsrow -Margin 2 -VisualStyle C1
-            #[System.Windows.Forms.MessageBox]::Show($name,"WinMerge",0,"Information");
             $tabsrow++  
         } 
 
@@ -39,11 +53,10 @@ New-Grid -Name WinMerge -ControlName 'Show-ISETab' -Rows $totalrows -Columns 2 -
                    
             Button "Ok" -On_Click { 
                     #Retrieve checkbox variables.
-                    #$checkboxes = get-variable ($tabs.Displayname -creplace '[^a-zA-Z0-9]', '')
                     $checkboxes = get-variable
-                    #[System.Windows.Forms.MessageBox]::Show($($checkboxes.name),"Selected",0,"Information"); 
                     $selected = ($checkboxes.Value | ? ischecked -eq "$true").count
-                    #[System.Windows.Forms.MessageBox]::Show($($selected),"Selected",0,"Information");           
+
+                    #Check if only 2 tabs in WPF form are selected.
                     if ($selected -ne 2)
                         {
                             [System.Windows.Forms.MessageBox]::Show("Only 2 checkboxes allowed!","WinMerge",0,"Warning");
@@ -55,19 +68,18 @@ New-Grid -Name WinMerge -ControlName 'Show-ISETab' -Rows $totalrows -Columns 2 -
                             if ($checkboxes.Value | ? ischecked -eq "$true")
                             {
                                 $mychecks = ($checkboxes.Value | ? ischecked -eq "$true").Name
-                                #[System.Windows.Forms.MessageBox]::Show($($mychecks),"MyChecks",0,"Information");
-                                $params
+                                [array]$params = @()
                                 foreach ($check in $mychecks)
                                 {
-                                    #[System.Windows.Forms.MessageBox]::Show($($check),"Check",0,"Information");
                                     $FullPath = ($tabs | ? {($_.Displayname -creplace '[^a-zA-Z0-9]', '') -eq $($check)})
-                                    [System.Windows.Forms.MessageBox]::Show($($FullPath.fullpath),"FullPath",0,"Information");
-                                    #add fullpath to params variable
-                                    $params += "'"+$FullPath.Fullpath+"'" + " "
+                                    $params += $FullPath.Fullpath
                                    
                                 }
-                                 #[System.Windows.Forms.MessageBox]::Show($($params),"Params",0,"Information");
-                                 Invoke-Expression "C:\Temp\WinMergePortable\WinMergePortable.exe $params"
+                                $param1 = [string]$params[0]
+                                $param2 = [string]$params[1]
+                                #Call WinMerge application
+                                $exe = "C:\Program Files (x86)\WinMerge\WinMergeU.exe"
+                                &$exe $param1 $param2                              
                                 
                             }
                             else
@@ -84,4 +96,3 @@ New-Grid -Name WinMerge -ControlName 'Show-ISETab' -Rows $totalrows -Columns 2 -
             } -IsDefault -Margin 5 -Row $totalrows -Column 1 #-ColumnSpan 2
         }
 }
-
