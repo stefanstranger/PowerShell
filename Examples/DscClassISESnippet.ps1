db29b581f26b53e6529f8d6e2c1d9fc8dd5232e6
Stefan Stranger <stefan@stranger.nl>
2015-07-23 09:31:05 +0200

    Dsc Class ISE Snippet
    Add Dsc Class Snippet to ISE

---
+++ Examples/DscClassISESnippet.ps1
@@ -0,0 +1,3 @@
+﻿New-IseSnippet -Title "DSC Class" -Author "Stefan Stranger" `
+-Description "DSC Class Snippet" `
+-Text (Get-Content .\DscClassIseSnippetCode.ps1 -Raw)
---
+++ Examples/DscClassISESnippetCode.ps1
@@ -0,0 +1,36 @@
+﻿enum Ensure
+{
+   Absent
+   Present
+}
+ 
+[DscResource()]
+class ResourceName
+{
+   [DscResourceKey()]
+   #Some Key Property
+ 
+   [DscResourceMandatory()]
+   #Some Mandatory property
+ 
+   #Use of enumeration
+   [Ensure] $Ensure
+ 
+   #Set function similar to Set-TargetResource
+   [void] Set()
+   {
+      #Set logic goes here
+   }
+ 
+   #Test function similar to Test-TargetResource
+   [bool] Test()
+   {
+      #Test logic goes here
+   }
+ 
+   #Get function similar to Get-TargetResource
+   [Hashtable] Get()
+   {
+      #Get logic goes here
+   }
+}
